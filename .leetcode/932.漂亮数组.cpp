b2da798d5301adaa11d6043633a6e4bdb6dee5c5
haifenggo <haifeng.zh@hotmail.com>
2022-09-20 12:52:08 +0800

    12:52

--- .leetcode/932.漂亮数组.cpp
+++ .leetcode/932.漂亮数组.cpp
@@ -7,8 +7,26 @@
 // @lc code=start
 class Solution {
 public:
+    map<int,vector<int>>mp;
     vector<int> beautifulArray(int n) {
-        
+        vector<int>ans;
+        if(n==1){
+            ans.push_back(1);
+            return ans;
+        }
+        if(mp.count(n)){return mp[n];}
+        int odd=(n+1)/2;
+        int even=n/2;
+        vector<int>left=beautifulArray(odd);
+        vector<int>right=beautifulArray(even);
+        for(auto& i:left){
+            ans.push_back(2*i-1);
+        }
+        for(auto& i:right){
+            ans.push_back(2*i);
+        }
+        mp[n]=ans;
+        return ans;
     }
 };
 // @lc code=end
