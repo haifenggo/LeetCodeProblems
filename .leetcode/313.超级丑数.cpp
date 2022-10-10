21923a9c4362f68720c3d76279ade34ab837f6b6
haifenggo <haifeng.zh@hotmail.com>
2022-10-10 13:10:04 +0800

    13:10

---
+++ .leetcode/313.超级丑数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=313 lang=cpp
+ *
+ * [313] 超级丑数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+
+    }
+};
+// @lc code=end
+
--- .leetcode/594.最长和谐子序列.cpp
+++ .leetcode/594.最长和谐子序列.cpp
@@ -8,7 +8,17 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-
+        unordered_map<int,int>mp;
+        int ans=0;
+        for(int& a:nums){
+            mp[a]++;
+        }
+        for(auto [k,v]:mp){
+            if(mp.count(k+1)){
+                ans=max(ans,v+mp[k+1]);
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
