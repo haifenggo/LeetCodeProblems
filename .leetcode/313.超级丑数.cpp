cf59b9bd2698ab93263f65fe845206d482724bcc
haifenggo <haifeng.zh@hotmail.com>
2022-10-11 20:28:17 +0800

    20:28

--- .leetcode/313.超级丑数.cpp
+++ .leetcode/313.超级丑数.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-
+        priority_queue<int,vector<int>,greater<int>>q;
+        q.push(1);
+        int x=1;
+        while(n--){
+            x=q.top();
+            q.pop();
+            for(int & a:primes){
+                if(a<=INT_MAX/x)q.push(x*a);
+                if(x%a==0)break;
+            }
+        }
+        return x;
     }
 };
 // @lc code=end
